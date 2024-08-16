1780da0fba8bc9104139e93e1e55de5ebad44fe2
glowworm <15159885771@163.com>
2024-08-16 18:05:19 +0800

    MultiLineTrace

--- UE/printC/myThirdProject01/Source/myThirdProject01/Public/MyCharacterCpp.h
+++ UE/printC/myThirdProject01/Source/myThirdProject01/Public/MyCharacterCpp.h
@@ -1,8 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
-#include "CoreMinimal.h"
 #include "InputAction.h"
 #include "Components/SphereComponent.h"
 #include "GameFramework/Character.h"
